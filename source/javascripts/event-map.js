c7a9377297d496a4499aab0edbd83d7d7b2206ae
Katherine Fellows <k@therinefello.ws>
2015-09-08 04:46:38 -0500

    Add logic for geocoding event locations
    ...if Google can't manage to geocode the event, it defaults to Brooklyn.
    Because why not?!

--- source/javascripts/event-map.js
+++ source/javascripts/event-map.js
@@ -1,12 +1,78 @@
+var geocoder;
+var map;
+var markers = [];
+
+// Creates infoWindow for a marker
+// Launches infoWindow when marker is clicked
+function addInfoWindow(marker, event) {
+  var contentString = '<div id="info-window"' +
+                      '<div id="siteNotice"' +
+                      '</div>' +
+                      '<h1 class="info-heading">' + event["location"] + '</h1>' +
+                      '<ul>' +
+                      '<li><b>Host:</b> ' + event["host"] + '</li>' +
+                      '<li><b>RSVP:</b> ' + event["rsvp"] + '</li>' +
+                      '</ul>' +
+                      '<center><a href="' + event["more-info"] + '">More Information</b></a><br><br>';
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
+}
+
+// Geocodes the given location 
+function codeAddress(data) {
+  geocoder.geocode({ 'address': data["location"] }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latlong = results[0].geometry.location;
+      console.log(latlong);
+
+      var marker = new google.maps.Marker({
+        position: latlong,
+        map: map,
+        title: data["title"]
+      });
+
+      markers.push(marker);
+
+      addInfoWindow(marker, data);
+    }
+  });
+}
+
+// Wipes all markers from the display
+function nullAllMarkers(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+}
+
+function updateMarkers() {
+  nullAllMarkers();
+
+  $.get('/events.json', {}, function(data) {
+    for (var i = 0; i < data.length; i++) {
+      codeAddress(data[i]);
+    }
+  });
+}
+
 // When the window has finished loading, create our Google map below
 google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
+    // Initializes geocoding with Google Maps API
+    geocoder = new google.maps.Geocoder();
+
     // Basic operations for a simple Google Map
     // For more options see:
     // https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
-        zoom: 11,
+        zoom: 2,
         center: new google.maps.LatLng(40.6700, -73.9400), // NYC
         styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
     };
@@ -14,12 +80,7 @@ function init() {
     // Get the HTML DOM element that will contain your map
     // We are using a div with id="event-map" seen below
     var mapElement = document.getElementById('event-map');
-    var map = new google.maps.Map(mapElement, mapOptions);
+    map = new google.maps.Map(mapElement, mapOptions);
 
-    // Add a marker because WHY NOT
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
-        map: map,
-        title: "Upcoming PLIBMTTBHGATYs"
-    });
+  updateMarkers();
 }
