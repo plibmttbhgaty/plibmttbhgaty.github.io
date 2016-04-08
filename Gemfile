6cf6543beef17827bf493ca5a712fe271ca5f242
Laila Winner <laila@thoughtbot.com>
2016-04-08 12:31:26 -0700

    Remove middleman-blog and upgrade middleman-deploy
    * We're not currently using any of the middleman-blog features. This
      commit removes middleman-blog from the Gemfile and removes the
      blog-related code.
    * Upgrade `middleman` for compatibility with `middleman-deploy`

--- Gemfile
+++ Gemfile
@@ -2,13 +2,9 @@
 # the following line to use "https"
 source "http://rubygems.org"
 
-gem "middleman", "~> 3.3.12"
-gem "middleman-blog", "~> 3.5.3"
+gem "middleman"
 gem "middleman-deploy", git: "https://github.com/middleman-contrib/middleman-deploy", branch: "master"
 gem "middleman-livereload"
-
-# For feed.xml.builder
-gem "builder", "~> 3.0"
 
 # XML parsing
 gem "nokogiri"
--- config.rb
+++ config.rb
@@ -1,37 +1,6 @@
-###
-# Blog settings
-###
-
 # Time.zone = "UTC"
 
 activate :autoprefixer
-
-activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
-end
-
-page "/feed.xml", layout: false
 
 ###
 # Compass
@@ -87,6 +56,11 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch = "master"
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -103,10 +77,6 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-end
-
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
 end
 
 ###
